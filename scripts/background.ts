0f9b5ac5eb03fce16d592dc8ec9401c7b71bc573
Tolu Lawson <lawsontolu1@gmail.com>
2025-12-01 01:55:00 -0800

    Update file permissions to executable and enhance URL handling in background script

--- scripts/background.ts
+++ scripts/background.ts
@@ -1,6 +1,8 @@
 chrome.action.onClicked.addListener((tab) => {
   if (tab.url && tab.id) {
-    const archiveUrl = `https://archive.ph/o/${tab.url}`;
+    const url = new URL(tab.url);
+    const cleanUrl = `${url.origin}${url.pathname}`;
+    const archiveUrl = `https://archive.ph/o/${cleanUrl}`;
     chrome.tabs.update(tab.id, { url: archiveUrl });
   }
 }); 
